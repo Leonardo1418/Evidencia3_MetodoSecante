032146c60b268c52b804d2ba60b8c194a5f3ff10
Leonardo <leoromo10@hotmail.com>
2025-05-14 01:34:42 -0700

    Modificaciones al codigo para mejor legibilidad

--- Evidencia3.py
+++ Evidencia3.py
@@ -1,43 +1,32 @@
 import math
 
+def Evaluar_Funcion(Funcion, x):
+    return eval(Funcion.replace("x", str(x)))
+
 Funcion = input("Ingrese la funcion a evaluar:\n f(x) = ")
-Funcion = Funcion.replace("^", "**")
-Funcion = Funcion.replace("e", str(math.e))
-Funcion = Funcion.lower()
+Funcion = Funcion.replace("^", "**").replace("e", str(math.e)).lower()
+
 Error = False
 i = 0
-Bucle = 0
 ter = 100
 
 xl = float(input("Ingrese el valor de xl/xi-1:\n xl = "))
 xu = float(input("Ingrese el valor de xu/xi:\n xu = "))
-
 tol = float(input("Ingrese la tolerancia deseada (%):\n tol = "))
 
 print(f'\nFuncion: f(x) = {Funcion}')
-print(f'{"iteracion":<10} {"xl":<10} {"xu":<10} {"fxl":<10} {"fxu":<10} {"xr":<10} {"ter":<10}')
-while tol<ter:
-    fxl = eval(Funcion.replace("x", str(xl)))
-    fxu = eval(Funcion.replace("x", str(xu)))
+print(f'{"iteracion":<12} {"xl":<12} {"xu":<12} {"fxl":<12} {"fxu":<12} {"xr":<12} {"ter":<12}')
+while ter > tol:
+    fxl = Evaluar_Funcion(Funcion, xl)
+    fxu = Evaluar_Funcion(Funcion, xu)
 
-    xr = xu-((fxu)*(xl-xu))/(fxl-fxu)
+    xr = xu - ((fxu) * (xl-xu)) / (fxl-fxu)
 
-    ter = abs((xu-xl)/xu)
+    ter = abs((xu-xl) / xu) * 100
 
-    print(f'{i:<10} {round(xl, 2):<10} {round(xu, 2):<10} {round(fxl, 2):<10} {round(fxu, 2):<10} {round(xr, 2):<10} {round(ter*100, 2) if Error==True else "-":<10}')
+    print(f'{i:<12} {round(xl, 6):<12} {round(xu, 6):<12} {round(fxl, 6):<12} {round(fxu, 6):<12} {round(xr, 6):<12} {round(ter, 6) if Error==True else "-":<12}%')
     Error = True
 
+    i +=1
     xl = xu
     xu = xr
-
-    i += 1
-
-if ter<=tol:
-    fxl = eval(Funcion.replace("x", str(xl)))
-    fxu = eval(Funcion.replace("x", str(xu)))
-
-    xr = xu-((fxu)*(xl-xu))/(fxl-fxu)
-
-    ter = abs((xu-xl)/xu)
-
-    print(f'{i:<10} {round(xl, 2):<10} {round(xu, 2):<10} {round(fxl, 2):<10} {round(fxu, 2):<10} {round(xr, 2):<10} {round(ter*100, 2) if Error==True else "-":<10}')
